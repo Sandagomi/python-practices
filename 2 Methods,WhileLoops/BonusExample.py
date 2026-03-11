c313617bc7657f59e8a6916af3414a63bbc4dbbe
sandagomi <sandagomi.v.i@gmail.com>
2026-03-11 14:56:38 +0530

    few exercises

--- 2 Methods,WhileLoops/BonusExample.py
+++ 2 Methods,WhileLoops/BonusExample.py
@@ -4,9 +4,19 @@ while len(x) <= 10:
     print(x)
     x = x + "*"
     
+    
+
 
 y = 1
 
 while y <= 10:
-    print(y)
+    print(x)
     y = y + 1
+    
+    
+
+a = 10
+
+while a >= 1:
+    print(x)
+    a = a - 1
