cedb9eb9ed59a65eb135bcfa8b9f628ed774cfbf
sandagomi <sandagomi.v.i@gmail.com>
2026-03-12 21:02:21 +0530

    exercise of the match case

---
+++ 3 Match-case,ForLoops/matCaseExercise.py
@@ -0,0 +1,15 @@
+country = "Italy"
+
+
+while True:
+    action = input("enter the country name: ")
+    
+    match action:
+        case "USA":
+            print("Hello")  
+        case "Italy":
+            print ("Ciao")
+        case "Germany":
+            print ("Hallo")
+        case "quit":
+            break   
