4fa0d5f042a0dd874946d1d4a2bf039eb9ec606e
sandagomi <sandagomi.v.i@gmail.com>
2026-03-20 21:29:54 +0530

    tuples and lists lesson

--- datamutation.py
+++ datamutation.py
@@ -3,3 +3,15 @@ fileNames = ["file1.txt", "file2.txt", "file3.txt"]
 for filename in fileNames:
     new = filename.replace(".", "-")
     print(new)
+    
+    
+    
+numbers = [1,2,3,4,5]
+
+numbers[0] = 10
+
+print(numbers)
+
+fruits = ("orange", "banana", "cherry")
+
+print(fruits)
