0c1fb998b5a44a285592a07b6dd1b2b344d39a25
sandagomi <sandagomi.v.i@gmail.com>
2026-03-23 08:24:35 +0530

    whats app messages automation

--- 4 List-Indexing,Tuples/randompwd.py
+++ 4 List-Indexing,Tuples/randompwd.py
@@ -1,10 +1,19 @@
+#Lets generate a random password
+
 import random
 import string
+
+#you can give any length
 
 def generate_password(length=12):
     characters = string.ascii_letters + string.digits + string.punctuation
     password = ''.join(random.choice(characters) for _ in range(length))
     return password
 
-# Generate password
-print("Your secure password:", generate_password(16))
+
+#now lets call the function and print the password
+
+print("Your password: ", generate_password(16)) #you can change the length as needed
+
+
+Your password:  x:!~Dpp~P2tzgbwi
